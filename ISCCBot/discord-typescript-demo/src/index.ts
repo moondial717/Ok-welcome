dd679b10842a964cc58fb81b330f7452a512a3d1
FrostRabbit <68796320+FrostRabbit@users.noreply.github.com>
2024-01-22 15:40:11 +0800

    Change js to ts

--- ISCCBot/discord-typescript-demo/src/index.ts
+++ ISCCBot/discord-typescript-demo/src/index.ts
@@ -3,13 +3,15 @@ import dotenv from 'dotenv'
 import { cleanEnv, str } from 'envalid'
 
 import { setBotListener } from './bot'
-import { PingSlashCommand } from './commands/ping'
+import { PingSlashCommand,testSlashCommand } from './commands/ping'
 import { deploySlashCommands } from './deploy'
-import { SlashCommand } from './types/command'
+import { SlashCommand,SlashCommand1 } from './types/command'
 import { AppConfig } from './types/config'
+import test from 'node:test'
 
 // Register commands
 const commandList: Array<SlashCommand> = [PingSlashCommand]
+const commandList1: Array<SlashCommand1> = [testSlashCommand]
 
 // Read .env file (if exist)
 dotenv.config()
@@ -25,7 +27,7 @@ const env = cleanEnv(process.env, {
 const appConfig: AppConfig = {
   token: env.TOKEN,
   clientId: env.CLIENT_ID,
-  guildId: env.GUILD_ID
+  guildIds: env.GUILD_ID.split(',')
 }
 
 // DiscordJS API Client: https://discord.js.org/#/docs/discord.js/main/class/Client
@@ -35,9 +37,13 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 deploySlashCommands(appConfig, commandList)
   .then((response) => console.log(`Deploy ${response.length} commands: ${response.map((c) => c.name)} successfully!`))
   .catch((reason) => console.log(`Failed to deploy commands: ${reason}`))
+deploySlashCommands(appConfig, commandList1)
+  .then((response) => console.log(`Deploy ${response.length} commands: ${response.map((c) => c.name)} successfully!`))
+  .catch((reason) => console.log(`Failed to deploy commands: ${reason}`))
 
 // Add event listener from discord
 setBotListener(client, commandList)
+setBotListener(client, commandList1)
 
 // Logs the client in, establishing a WebSocket connection to Discord.
 client
--- ISCCBot/discord-typescript-demo/src/types/command.ts
+++ ISCCBot/discord-typescript-demo/src/types/command.ts
@@ -1,6 +1,10 @@
-import { CommandInteraction,SlashCommandBuilder } from 'discord.js'
+import { CommandInteraction,ChatInputCommandInteraction,SlashCommandBuilder } from 'discord.js'
 
 export interface SlashCommand {
   data: SlashCommandBuilder
   execute: (interaction: CommandInteraction) => Promise<void>
+}
+export interface SlashCommand1 {
+  data: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>
 }
